4bffd35e166b4045f20d5d65171ac18b58478117
renato <renato.henrique_98@hotmail.com>
2022-09-01 23:12:15 -0300

    feat: create project

---
+++ service-b/src/main/java/com/github/throyer/rabbitmq/RabbitmqPocApplication.java
@@ -0,0 +1,13 @@
+package com.github.throyer.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqPocApplication.class, args);
+	}
+
+}
