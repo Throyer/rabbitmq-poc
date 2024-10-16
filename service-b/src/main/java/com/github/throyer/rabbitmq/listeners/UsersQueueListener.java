4cc1b47a7731b8e261eeb687181042db6be5839a
Renato <renato.henrique_98@hotmail.com>
2024-10-16 13:40:57 -0300

    improve constructor

--- service-b/src/main/java/com/github/throyer/rabbitmq/listeners/UsersQueueListener.java
+++ service-b/src/main/java/com/github/throyer/rabbitmq/listeners/UsersQueueListener.java
@@ -4,25 +4,15 @@ import com.github.throyer.rabbitmq.dtos.UserDto;
 import com.github.throyer.rabbitmq.services.CreateUserService;
 import com.github.throyer.rabbitmq.shared.*;
 import com.github.throyer.rabbitmq.utils.JSON;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@AllArgsConstructor
 public class UsersQueueListener implements SimpleRetryListener<UserDto> {
-
-  public UsersQueueListener(
-    RabbitMQProperties settings,
-    CreateUserService service,
-    @Qualifier("rabbitmq-template") AmqpTemplate rabbitmq
-  ) {
-    this.settings = settings;
-    this.service = service;
-    this.rabbitmq = rabbitmq;
-  }
-
   private final RabbitMQProperties settings;
   private final CreateUserService service;
 	private final AmqpTemplate rabbitmq;
