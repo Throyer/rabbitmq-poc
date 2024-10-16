e36518cd1d40787e79eb6f3f7e48a08bc4dfb951
Renato Henrique <renato.henrique_98@hotmail.com>
2024-10-16 00:31:34 -0300

    import

--- service-b/src/main/java/com/github/throyer/rabbitmq/listeners/UsersQueueListener.java
+++ service-b/src/main/java/com/github/throyer/rabbitmq/listeners/UsersQueueListener.java
@@ -3,7 +3,6 @@ package com.github.throyer.rabbitmq.listeners;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.github.throyer.rabbitmq.models.User;
